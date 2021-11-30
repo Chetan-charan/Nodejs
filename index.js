807af06cd5db088da98fdcd11a1e0ccf2e4dbee1
Chetan-charan <chetanharicharan@gmail.com>
2021-11-30 15:24:00 +0530

    dotenv

--- index.js
+++ index.js
@@ -1,99 +1,84 @@
-const express = require("express");
+import  express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
 
+dotenv.config();
+console.log(process);
 const app = express()
 
 const PORT = 9000
 
-const movies = [{
-   
-    "id" : "101",
-    "name" : "No Country for Old Men",
-    "poster" : "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
-    "rating" : 8.1,
-    "summary" : "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
-    "trailer" : "https://www.youtube.com/embed/38A__WT3-o0",
-    "langauge" : "English",
-},
-{
-    "id" : "102",
-    "name" : "Jai Bhim",
-    "poster" : "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
-    "summary" : "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
-    "rating" : 8.8,
-    "trailer" : "https://www.youtube.com/embed/nnXpbTFrqXA",
-    "langauge" : "Tamil",
-},
-{
-   
-    "id" : "103",
-    "name" : "The Avengers",
-    "rating" : 8,
-    "summary" : "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
-    "poster" : "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
-    "trailer" : "https://www.youtube.com/embed/eOrNdBpGMv8",
-    "langauge" : "English",
-},
-{
-    "id" : "104",
-    "name" : "Interstellar",
-    "poster" : "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
-    "rating" : 8.7,
-    "summary" : "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
-    "trailer" : "https://www.youtube.com/embed/zSWdZVtXT7E",
-    "langauge" : "English",
-},
-{
-    "id" : "105",
-    "name" : "Baahubali",
-    "poster" : "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
-    "rating" : 8.7,
-    "summary" : "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
-    "trailer" : "https://www.youtube.com/embed/sOEg_YZQsTI",
-    "langauge" : "Telugu",
-},
-{
+app.use(express.json())    //every request in the app body will be parsed as json
 
-    "id" : "106",
-    "name" : "Ratatouille",
-    "poster" : "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
-    "rating" : 8,
-    "summary" : "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
-    "trailer" : "https://www.youtube.com/embed/NgsQ8mVkN8w",
-    "langauge" : "English",
-}]
+const MONGO_URL = process.env.MONGO_URL;   //only line to be changed to make the database online(atlas)
 
+//mongodb+srv://chetan:<password>@cluster0.hz7iv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+async function createConnection(){                          //creating mongodb connection
+    const client = new MongoClient(MONGO_URL);
+    await client.connect();
+    console.log("Mongodb connected");
+    return client;
+}
+
+const client = await createConnection();
 
 app.get('/',(req,res) => {
     res.send("Hello  🌎 !!!")
 })
 
-
-app.get('/movies',(request,response) => {
-  
+app.get('/movies',async (req,res) => {
    
-    const {langauge,rating} =  request.query;                                //using object destructuring
-  
-    let filterMovies = movies;                                               //initial value assigned to full array
-
-    if(langauge){
-      filterMovies = filterMovies.filter((mv) => mv.langauge == langauge);        //if language is true value, filter above value by langauge
+    let filter = req.query;     //json object of query parameters
+    if(filter.rating){
+        filter.rating = parseFloat(filter.rating);
     }
-    if(rating){
-        
-        filterMovies = filterMovies.filter((mv) => mv.rating == rating);           //if rating is a true value, filter above value by rating also
-    }
-    response.send(filterMovies)
+    console.log(filter);
+    const  filterMovies = await client.db("b28wd").collection("movies").find(filter).toArray();
+    res.send(filterMovies);
     
 })
 
-app.get('/movies/:id',(req,res) => {
+app.get('/movies/:id',async (req,res) => {
   
     const { id } = req.params;                                        //getting id using object destructuring
-
-    const movie = movies.find((mv) => mv.id === id);                  //using find method to get a single movie
-
+    const movie = await getMoviebyId(id)      //using code refactoring so that the code can be reused   //pass id to search
+    console.log(movie);
     movie ? res.send(movie) : res.send("No match found")              
     
 })
 
+app.post("/movies", async (req,res)=> {
+    const data = req.body;                            //data is given from postman -> body -> raw -> Json
+    const result = await client.db("b28wd").collection("movies").insertMany(data);    
+    res.send(result);
+})
+
+app.put("/movies/:id",async (req,res) => {
+    const { id } = req.params;
+    const data = req.body;
+    const result = await updateMoviebyId(id, data);
+    const movie = await getMoviebyId(id);
+    result.modifiedCount > 0 ? res.send(movie) : res.status(404).send({ message: "No Movie Found" });
+})
+
+app.delete("/movies/:id",async (req,res) => {
+    const { id } = req.params;
+    const result = await deleteMoviebyId(id);
+    result.deletedCount > 0 ? res.send(result) : res.status(404).send({ message: "No Matching movie found" });   
+    
+})
+
 app.listen(PORT, () => console.log("App is started in Port",PORT));
+
+async function deleteMoviebyId(id) {
+    return await client.db("b28wd").collection("movies").deleteOne({ id: id });
+}
+
+async function updateMoviebyId(id, data) {
+    return await client.db("b28wd").collection("movies").updateOne({ id: id }, { $set: data });
+}
+
+async function getMoviebyId(id) {
+    return await client.db("b28wd").collection("movies").findOne({ id: id });
+}
