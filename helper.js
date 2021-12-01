d59cdb9fcfa43a996dd3fe2434952468f81e4ed1
Chetan-charan <chetanharicharan@gmail.com>
2021-12-01 18:37:51 +0530

    import object id

--- helper.js
+++ helper.js
@@ -1,11 +1,11 @@
-import { ObjectId } from "bson";
+import { ObjectId } from "mongodb";
 import { client } from "./index.js";
 
  async function createMovies(data) {
     return await client.db("b28wd").collection("movies").insertMany(data);
 }
  async function deleteMoviebyId(id) {
-    return await client.db("b28wd").collection("movies").deleteOne({ id: ObjectId(id) });
+    return await client.db("b28wd").collection("movies").deleteOne({ id: ObjectId(id) });    //converts the id into objectId
 }
  async function updateMoviebyId(id, data) {
     return await client.db("b28wd").collection("movies").updateOne({ _id: ObjectId(id)  }, { $set: data });
